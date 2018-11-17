421ee3497f13ab422ba2fe97952bcc96c8ca9802
Rodrigo Dominguez <rorra@rorra.com.ar>
2018-11-17 03:36:50 -0200

    Chapter 9 done

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const ctrlLocations = require('../controllers/locations');
+//const ctrlLocations = require('../controllers/locations');
 const ctrlOthers = require('../controllers/others');
 
 /* Location pages */
-router.get('/', ctrlLocations.homelist);
-router.get('/location/:locationid', ctrlLocations.locationInfo);
-router.get('/location/:locationid/review/new', ctrlLocations.addReview);
-router.post('/location/:locationid/review/new', ctrlLocations.doAddReview);
+router.get('/', ctrlOthers.angularApp);
+// router.get('/location/:locationid', ctrlLocations.locationInfo);
+// router.get('/location/:locationid/review/new', ctrlLocations.addReview);
+// router.post('/location/:locationid/review/new', ctrlLocations.doAddReview);
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
